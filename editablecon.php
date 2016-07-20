274e0d498d4da8aefd9555ab0515cba497d7a354
malekian <hadi.malekian@outlook.com>
2016-07-20 21:37:37 +0430

    Updated code

--- editablecon.php
+++ editablecon.php
@@ -6,6 +6,7 @@
 <!--check login connection-->
 <?php
 error_reporting(0);
+$saltt=$_COOKIE['c_salt'];
 $db_host="localhost";
 $db_user="root";
 $db_pass="";
@@ -13,9 +14,13 @@ $db_name="realestate";
 $db_table="signup";
 $connect = mysql_connect("$db_host", "$db_user", "$db_pass") or die("could not connect to the server");
 mysql_select_db("realestate",$connect) or die("could not connect to the database");
+$check = mysql_fetch_array(mysql_query("SELECT * FROM `signup` WHERE `salt`='$saltt'"));
+$username=$check['username'];
+echo"$username";
 include "algor.php";
 if ($logged==true){
 	?>
+	
 	<form action="" method="POST" >
   apartmentadress:<br>
   <input type="text" name="apartmentadress"><br>
@@ -51,18 +56,15 @@ mysqli_query($connect,"SET character_set_connection='utf8'");
 if(mysqli_connect_errno()){
 	die("unable to connect".mysqli_connect_errno());
 }
-?>
-<!--insert data to database-->
-<?php
 $apartmentadress = !empty($_POST['apartmentadress']) ? $_POST['apartmentadress'] : '';	
 $case = !empty($_POST['case']) ? $_POST['case'] : '';
 $price = !empty($_POST['price']) ? $_POST['price'] : '';
 $name = !empty($_POST['name']) ? $_POST['name'] : '';	
 $phone = !empty($_POST['phone']) ? $_POST['phone'] : '';
 if(isset($_POST['submit'])){
-if ($stmt = mysqli_prepare($connect, "INSERT INTO $db_table VALUES ('', ?, ?, ?, ?, ?)"))
-mysqli_stmt_bind_param($stmt, "sssss",
-$apartmentadress,$case,$price,$name,$phone);
+if ($stmt = mysqli_prepare($connect, "INSERT INTO $db_table VALUES ('', ?, ?, ?, ?, ? ,?)"))
+mysqli_stmt_bind_param($stmt, "ssssss",
+$apartmentadress,$case,$price,$name,$phone,$username);
 if(mysqli_stmt_execute($stmt))
     {
 		$alert='<p>Information was sent</p>';
