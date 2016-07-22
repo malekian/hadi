8588977bb4fbf72d2c9acb91930315f235617bb5
malekian <hadi.malekian@outlook.com>
2016-07-22 10:44:38 +0430

    Updated code

--- edit_delete.php
+++ edit_delete.php
@@ -22,24 +22,65 @@ $result = mysqli_query($connect,"SELECT*FROM ".$db_table." WHERE id = $ID");
 while($row = mysqli_fetch_array($result)):
 $id= $row['id'];
 $address=$row['address'];
-echo"$address";
-echo"<br>";
 $caseee=$row['caseee'];
-echo"$caseee";
-echo"<br>";
 $price=$row['price'];
-echo"$price";
-echo"<br>";
 $name=$row['name'];
-echo"$name";
-echo"<br>";
 $phone=$row['phone'];
-echo"$phone";
-echo"<br>";
 $username=$row['username'];
-echo"$username";
-echo"<br>";
 endwhile;
 ?>  
+<form action="" method="POST" >
+  apartmentadress:<br>
+  <input type="text" name="apartmentadress" value="<?php echo "$address"; ?>"><br>
+  case:<br>
+  <select name="case">
+  <option value="">please choose</option>
+  <option value="sell">sell</option>
+  <option value="rent">rent</option>
+  </select><br>
+  apartment price:<br>
+  <input  name="price" value="<?php echo "$price"; ?>"></br>
+  family name:<br>
+  <input type="text" name="name" value="<?php echo "$name"; ?>"><br>
+  phone number:<br>
+  <input name="phone" value="<?php echo "$phone"; ?>"><br>
+  <p><input name="delete" type="submit" id="submit" value="delete" /></p>
+   <p><input name="edit" type="submit" id="submit" value="edit" /></p>
+</form>
+<?php
+if(isset($_POST['delete'])){
+$sql = "DELETE FROM advertisement WHERE `id`='$id'";
+
+if ($connect->query($sql) === TRUE) {
+header("Location: submitmassage.php");
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$connect->close();
+}
+?>
+<?php
+$apartmentadress = !empty($_POST['apartmentadress']) ? $_POST['apartmentadress'] : '';
+$price = !empty($_POST['price']) ? $_POST['price'] : '';
+$name = !empty($_POST['name']) ? $_POST['name'] : '';	
+$phone = !empty($_POST['phone']) ? $_POST['phone'] : '';
+if(isset($_POST['edit'])){
+$sql = "UPDATE advertisement SET
+address='$apartmentadress',
+price='$price',
+name='$name',
+phone='$phone'
+WHERE `id`='$id'";
+
+if ($connect->query($sql) === TRUE) {
+header("Location: submitmassage.php");
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$connect->close();
+}
+?>
 </body>
 </html>
