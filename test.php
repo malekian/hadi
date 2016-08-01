5ee844bb36092c786e426f93694a1bee537fe1b5
malekian <hadi.malekian@outlook.com>
2016-08-01 14:44:03 +0430

    Updated code

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+<?php
+if((isset($_POST["submit"]))&&(!empty($_FILES['fileToUpload'])) ){
+foreach($_FILES['fileToUpload']['name'] as $key => $name){
+ $filename = $_FILES['fileToUpload']['name'][$key];
+echo"$filename";
+	}
+}
+?>
