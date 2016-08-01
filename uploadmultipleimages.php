6e731a233c722490b5880d86d623185f7157f175
Slayther <slayther.morderclaw@gmail.com>
2016-08-01 16:36:45 +0200

    Updated code

--- uploadmultipleimages.php
+++ uploadmultipleimages.php
@@ -1,25 +1,153 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>upload multiple image</title>
+  <meta charset="utf-8">
+  <title>upload multiple image</title>
+  <style>
+    #result-container-template {
+      display: none;
+    }
+    
+    .button-style::-webkit-file-upload-button {
+      visibility: hidden;
+    }
+    
+    .button-style {
+      display: inline-block;
+      background: -webkit-linear-gradient(top, #f9f9f9, #e3e3e3);
+      border: 1px solid #999;
+      border-radius: 3px;
+      padding: 5px 8px;
+      outline: none;
+      white-space: nowrap;
+      -webkit-user-select: none;
+      cursor: pointer;
+      text-shadow: 1px 1px #fff;
+      font-weight: 700;
+      font-size: 10pt;
+    }
+    
+    .button-style:hover {
+      border-color: black;
+    }
+    
+    .button-style:active {
+      background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9);
+    }
+    
+    #choose-file {
+      display: none;
+    }
+    
+    #result-container {
+      display: inline-block;
+      width: 150px;
+      height: 172px;
+      margin: 1px;
+    }
+    
+    #image-container {
+      width: 100%;
+      height: 150px;
+    }
+    
+    #image-container img {
+      width: 100%;
+      height: 100%;
+      border-radius: 0px 0px 3px 3px;
+    }
+    
+    #result-container button {
+      width: 100%;
+      height: 22px;
+      border: transparent;
+      background-color: red;
+      color: white;
+      border-radius: 3px 3px 0px 0px;
+    }
+  </style>
 </head>
+
 <body>
-<form action="" method="POST" enctype="multipart/form-data" name="contactform" id="contactform"  autocomplete="on"> 
-<div>
-            <fieldset>
-            <legend>upload your image</legend><br><br><br><br>
-		    <p dir="rtl"><label>iamge</label>
-	        <input type="file" name="image[]" multiple><br><br>
-			</fieldset>
-</div>
-<div>			
-			<fieldset>
-			<p dir="rtl"><input name="submit" type="submit" class="submit" id="submit" value="upload" /></p>
-	        </fieldset>	
-</div>	
-</form>
-<?php
+  <div>
+    <fieldset>
+      <div id="result">
+        <div id="result-container-template">
+          <button class="remove_pict">remove</button>
+          <div id="image-container">
+            <img class="thumbnail" src="" title="undefined">
+          </div>
+        </div>
+      </div>
+      <legend>Upload your image</legend><br><br><br><br>
+      <p dir="rtl"><label class="button-style" for="choose-file">Add images</label>
+        <input id="choose-file" type="file" name="image[]" multiple=""><br><br>
+      </p>
+    </fieldset>
+  </div>
+  <form action="" method="POST" enctype="multipart/form-data" name="contactform" id="contactform" autocomplete="on">
+    <div>
+      <fieldset>
+        <p dir="rtl"><input class="button-style submit" name="submit" type="submit" id="submit" value="Upload"></p>
+      </fieldset>
+    </div>
+  </form>
+  <script>
+    window.onload = function () {
+
+      var template_div = document.querySelector("#result-container-template");
+
+      //Check File API support
+      if (window.File && window.FileList && window.FileReader) {
+        var filesInput = document.querySelector("input[type=file]");
+
+        filesInput.addEventListener("change", function (event) {
+
+          var files = event.target.files; //FileList object
+          var output = document.getElementById("result");
+
+          for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+
+            //Only pics
+            if (!file.type.match('image'))
+              continue;
+
+            var picReader = new FileReader();
+
+            picReader.addEventListener("load", function (event) {
+
+              var picFile = event.target;
+              var div = template_div.cloneNode(true);
+              div.id = "result-container";
+
+              var img_elem = div.querySelector("img");
+              img_elem.src = picFile.result;
+              img_elem.title = picFile.name;
+
+              output.insertBefore(div, null);
+              div.querySelector("button")
+                .addEventListener("click", function (event) {
+                  div.parentNode.removeChild(div);
+                });
+
+            });
+
+            //Read the image
+            picReader.readAsDataURL(file);
+          }
+
+          filesInput.value = "";
+        });
+      }
+      else {
+        console.log("Your browser does not support File API");
+      }
+    }
+
+  </script>
+  <?php
 $db_host="localhost";
 $db_user="root";
 $db_pass="";
@@ -68,7 +196,8 @@ if(isset($_POST['submit'])){
 if ($stmt = mysqli_prepare($connect, "INSERT INTO $db_table VALUES ('', ?)"))
 {
 	/* bind parameters for markers */
-    mysqli_stmt_bind_param($stmt, "s", implode($image, ","));
+	$image_names = implode($image, ",");
+    mysqli_stmt_bind_param($stmt, "s", $image_names);
     /* execute query */
     if(mysqli_stmt_execute($stmt))
     {
@@ -85,4 +214,5 @@ echo"your image has been send successfully";
 }
 ?>
 </body>
+
 </html>
